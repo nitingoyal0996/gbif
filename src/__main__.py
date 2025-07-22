6e3baadf6d3dcc4799b290d9ae9c70da65e5045c
Nitin Goyal <nitin.goyal@ufl.edu>
2025-07-22 18:19:49 +0000

    Debug logger issue on remote

--- src/__main__.py
+++ src/__main__.py
@@ -2,11 +2,11 @@ from .agent import GBIFAgent
 from ichatbio.server import run_agent_server
 from src.log import setup_logger
 
-# Force logger initialization
-logger = setup_logger()
-logger.info("TEST: Logging system startup check")
-print("DEBUG: Logging initialized, test message logged")
 
 if __name__ == "__main__":
+    # Force logger initialization
+    logger = setup_logger()
+    logger.info("TEST: Logging system startup check")
+    print("DEBUG: Logging initialized, test message logged")
     gbif = GBIFAgent()
     run_agent_server(gbif, host="0.0.0.0", port=9999)
